8449608d67b17b96302b7fe9af7252aede93e2d0
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-04-14 10:01:20 +0000

    Endpoints for GET requests

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("Hello, World!");
+// Import
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 8080;
+
+// Listener für express-instanz
+app.listen(
+    PORT,
+    () => console.log(`server ready at http://localhost"${PORT}`)
+);
+
+/** GET */
+
+/* 200 OK */ 
+app.get('/api/test', (req, res) => {
+    res.sendStatus(200);
+});
+
+/* JSON direct*/ 
+app.get("/api/json1", (req,res) =>{
+        res.send({ "key": "value" })
+});
+
+/* JSON ressource*/ 
+app.get('/api/json2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'test.json'));
+});
+
+/* HTML */ 
+app.get('/api/html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'boo.html'));
+});
+
+/* JPG */ 
+app.get('/api/jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', '404.jpg'));
+});
+
